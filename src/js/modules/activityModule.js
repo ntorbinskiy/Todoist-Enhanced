7b113b140ee282156488f3bf962c5d60501749d4
ntorbinskiy <n.torbinskiy@gmail.com>
2023-02-05 12:27:30 +0200

    Add break line

--- src/js/modules/activityModule.js
+++ src/js/modules/activityModule.js
@@ -10,6 +10,7 @@ const getItemScore = (name, regex) => {
 const getItemsScores = (tasks, getItemScore, regexForScoreAndPoints) => {
   return tasks.map((task) => {
     const taskChildNodes = [...task.childNodes];
+
     return taskChildNodes
       .map((taskChildNode) => {
         const svgPathOfItemChildNode =
